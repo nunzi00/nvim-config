5f5f0d89b984c7a0200e2aafd90f73eb7575aeb8
Lucas <nunzi00@gmail.com>
2022-11-27 23:48:34 +0100

    bump

--- nvim/lua-old/plugins/php-refactoring.lua
+++
@@ -1,4 +0,0 @@
-vim.g['vim_php_refactoring_default_property_visibility'] = 'private'
-vim.g['vim_php_refactoring_default_method_visibility'] = 'private'
-vim.g['vim_php_refactoring_auto_validate_visibility'] = 1
-vim.g['vim_php_refactoring_phpdoc'] = "pdv#DocumentCurrentLine"
--- nvim/lua-old/plugins/phpcs.lua
+++
@@ -1,1 +0,0 @@
---vim.opt.Vimphpcs_Standard='PSR12'
